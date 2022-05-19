386559c924e5c27f2a88fb7f88f6af79d0fef7d3
AnkitDevla <98605699+AnkitDevla@users.noreply.github.com>
2022-05-19 08:59:30 +0530

    assignments compelet

--- assign1.py
+++ assign1.py
@@ -1,16 +1,24 @@
-# working of try()
-x = int(input("Enter the number: "))
-y = int(input("Enter the number: "))
+# # working of try()
 
-def divide(x, y):
-	try:
-		# Floor Division : Gives only Fractional
-		result = x // y
-	except ZeroDivisionError:
-		print("Sorry ! You are dividing by zero ")
-	else:
-		print("Yeah ! Your answer is :", result)
-	finally:    
-		print('Program Completed')
 
-divide(x,y)
+
+try:
+    # Floor Division : Gives only Fractional
+    x = int(input("Enter the number: "))
+    y = int(input("Enter the number: "))
+    result = x // y
+    result = x // d
+
+except ZeroDivisionError:
+    print("Sorry ! You are dividing by zero ")
+
+except NameError:
+    print("Name is not defined")
+
+except ValueError:
+    print("Please Enter the Integer Value")
+else:
+    print("Yeah ! Your answer is :", result)
+finally:    
+    print('Program Completed,Nothing to Pending')
+
